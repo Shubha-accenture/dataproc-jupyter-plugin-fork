2ee8d64f861cda1e557acdf56e5f8e5cd3a40f36
aditee-accenture <aditee.abhijit.katti@accenture.com>
2024-06-28 11:37:08 +0530

    multiple nodes changes

--- src/scheduler/schedulerForm.tsx
+++ src/scheduler/schedulerForm.tsx
@@ -3,9 +3,9 @@ import { Input } from '../controls/MuiWrappedInput';
 import LabelProperties from '../jobs/labelProperties';
 import { eventEmitter } from '../utils/signalEmitter';
 
-function SchedulerForm({id, data }: any) {
+function SchedulerForm({ id, data }: any) {
   const [inputFileSelectedLocal, setInputFileSelectedLocal] = useState('');
-  const [inputFileSelected, setInputFileSelected] = useState( '');
+  const [inputFileSelected, setInputFileSelected] = useState('');
   const [retryCount, setRetryCount] = useState<number | undefined>(2);
   const [retryDelay, setRetryDelay] = useState<number | undefined>(5);
   const [parameterDetail, setParameterDetail] = useState(['']);
@@ -15,9 +15,12 @@ function SchedulerForm({id, data }: any) {
   const [duplicateKeyError, setDuplicateKeyError] = useState(-1);
 
   const onInputFileNameChange = (evt: any) => {
-    setInputFileSelectedLocal(evt.target.value);
-    eventEmitter.emit(`uploadProgress`, evt, data, setInputFileSelected);
-    console.log(inputFileSelected);
+    const file = evt.target.files && evt.target.files[0];
+    if (file) {
+      setInputFileSelectedLocal(evt.target.value);
+      eventEmitter.emit(`uploadProgress`, evt, data, setInputFileSelected);
+      console.log(inputFileSelected);
+    }
   };
 
   const handleRetryCountChange = (e: number) => {
@@ -31,51 +34,53 @@ function SchedulerForm({id, data }: any) {
   };
 
   useEffect(() => {
-      setRetryCount(data.retryCount);
-      setRetryDelay(data.retryDelay)
-      //data.parameter = parameterDetail;
+    setInputFileSelectedLocal(data.inputFile);
+    setRetryCount(data.retryCount);
+    setRetryDelay(data.retryDelay);
+    //data.parameter = parameterDetail;
   }, [data]);
 
   return (
     <>
       <div className="notebook-node">
-          <form>
-            <div className="custom-node__body">
-              <label htmlFor="file-input">Input file*</label>
-              <input
-                className="nodrag"
-                value={inputFileSelectedLocal}
-                type="file"
-                onChange={e => onInputFileNameChange(e)}
-              />
-              <Input
-                className="nodrag"
-                value={retryCount}
-                Label="Retry Count"
-                onChange={e => handleRetryCountChange(Number(e.target.value))}
-              />
-              <Input
-                className="nodrag"
-                value={retryDelay}
-                Label="Retry Delay"
-                onChange={e => handleRetryDelayChange(Number(e.target.value))}
-              />
-              <LabelProperties
-                labelDetail={parameterDetail}
-                setLabelDetail={setParameterDetail}
-                labelDetailUpdated={parameterDetailUpdated}
-                setLabelDetailUpdated={setParameterDetailUpdated}
-                buttonText="ADD PARAMETER"
-                keyValidation={keyValidation}
-                setKeyValidation={setKeyValidation}
-                valueValidation={valueValidation}
-                setValueValidation={setValueValidation}
-                duplicateKeyError={duplicateKeyError}
-                setDuplicateKeyError={setDuplicateKeyError}
-                fromPage="react-flow"
-              />
-            </div>
-          </form>
+        <form>
+          <div className="custom-node__body">
+            <label htmlFor="file-input">Input file*</label>
+            <input
+              className="nodrag"
+              type="file"
+              value={inputFileSelectedLocal}
+              onChange={e => onInputFileNameChange(e)}
+            />
+            {<div>{inputFileSelectedLocal}</div>}
+            <Input
+              className="nodrag"
+              value={retryCount}
+              Label="Retry Count"
+              onChange={e => handleRetryCountChange(Number(e.target.value))}
+            />
+            <Input
+              className="nodrag"
+              value={retryDelay}
+              Label="Retry Delay"
+              onChange={e => handleRetryDelayChange(Number(e.target.value))}
+            />
+            <LabelProperties
+              labelDetail={parameterDetail}
+              setLabelDetail={setParameterDetail}
+              labelDetailUpdated={parameterDetailUpdated}
+              setLabelDetailUpdated={setParameterDetailUpdated}
+              buttonText="ADD PARAMETER"
+              keyValidation={keyValidation}
+              setKeyValidation={setKeyValidation}
+              valueValidation={valueValidation}
+              setValueValidation={setValueValidation}
+              duplicateKeyError={duplicateKeyError}
+              setDuplicateKeyError={setDuplicateKeyError}
+              fromPage="react-flow"
+            />
+          </div>
+        </form>
       </div>
     </>
   );
