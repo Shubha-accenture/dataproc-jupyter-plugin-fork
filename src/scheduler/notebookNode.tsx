7268d0f8a4dac3747f2e8621d96a5debb80a6696
aditee-accenture <aditee.abhijit.katti@accenture.com>
2024-08-09 11:44:22 +0530

    css changes

--- src/scheduler/notebookNode.tsx
+++ src/scheduler/notebookNode.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Handle, NodeProps, Position } from 'reactflow';
 
 import { eventEmitter } from '../utils/signalEmitter';
@@ -16,77 +16,85 @@ const iconSaveToBigQuery = new LabIcon({
   svgstr: saveToBigQueryIcon
 });
 
-function NotebookNode({ id, data, selected,isConnectable }: NodeProps) {
-  const [clickedNodeId, setClickedNodeId]=useState('');
+function NotebookNode({ id, data, selected, isConnectable }: NodeProps) {
+  const [clickedNodeId, setClickedNodeId] = useState('');
   const [isNodeClicked, setIsNodeClicked] = useState(false);
-  const nodeLabel = data.inputFile ? `${id}.${data.inputFile}` : `${id}.${data.nodeType} Node`;
+  const nodeLabel = data.inputFile ? `${id}.${data.inputFile}` : `${id}.New NodeType`;
 
-  // const [status, setStatus] = useState('');
+  const [status, setStatus] = useState('');
 
   //console.log(data)
   const handleNodeClick = () => {
-    setClickedNodeId(id)
+    setClickedNodeId(id);
     setIsNodeClicked(true);
-    console.log("clicked node in notebook",clickedNodeId)
+    console.log('clicked node in notebook', clickedNodeId);
     eventEmitter.emit(`nodeClick`, id, isNodeClicked);
+    console.log(data.nodeType, data.inputFile);
   };
 
-  // eventEmitter.on('color coding', value: string, id:string) => {
-  //   console.log(status,id);
-  //   setStatus(value);
-  // });
-  // eventEmitter.on('nodeType', (value: string, nid: string) => {
-  //   if (id === nid) {
-  //     setNodeType(value);
-  //   }
-  // }); 
-
+  useEffect(() => {
+    console.log('inside useEffect');
+    if (
+      data &&
+      data.nodeType === 'Cluster' &&
+      data.inputFile !== '' &&
+      data.clusterName !== ''
+    ) {
+      setStatus('complete');
+      console.log(status);
+    }
+  }, []);
   return (
     <>
-    <div  onClick={handleNodeClick}>
-      <div className={selected ? "selected-node":"notebook-node"}>
-        <div
-          className= "box black" 
-          // {`box ${
-          //   status === 'complete'
-          //     ? 'green'
-          //     : status === 'incomplete'
-          //     ? 'orange'
-          //     : 'black'
-          // }`}
-        />
-        <Handle
-          type="target"
-          id="a"
-          position={Position.Top}
-          isConnectable={false}
-        />
-        <div className="node-content">
-          <div>
-            {(data.nodeType === 'Serverless' || data.nodeType === 'Cluster') && (
-              <iconCalendarRange.react
-                tag="div"
-                className="logo-alignment-react-flow"
-              />
-            )}
-            {data.nodeType === 'sql' && (
-              <iconSaveToBigQuery.react
-                tag="div"
-                className="logo-alignment-react-flow"
-              />
-            )}
+      <div onClick={handleNodeClick}>
+        <div className={selected ? 'selected-node' : 'notebook-node'}>
+          <div
+            className={`box ${
+              status === 'complete'
+                ? 'green'
+                : status === 'incomplete'
+                ? 'orange'
+                : 'black'
+            }`}
+          />
+          <Handle
+            type="target"
+            id="a"
+            position={Position.Top}
+            isConnectable={false}
+          />
+          <div className="node-content">
+            <div className="node-parent">
+              <div className="node-logo">
+              {(data.nodeType === 'Serverless' ||
+                data.nodeType === 'Cluster') && (
+                <iconCalendarRange.react
+                  tag="div"
+                  className="logo-alignment-react-flow"
+                />
+              )}
+              {data.nodeType === 'sql' && (
+                <iconSaveToBigQuery.react
+                  tag="div"
+                  className="logo-alignment-react-flow"
+                />
+              )}
+              </div>
+              <div className="node-header">
+                {id === '0' ? 'Trigger Node ' : nodeLabel}
+              </div>
+            </div>
+            <div className="node-subheader">
+                Run Now
+              </div>
           </div>
-          <div className="custom-node__header">
-            {id === '0' ? 'Trigger Node ' : nodeLabel}
-          </div>
+          <Handle
+            type="source"
+            position={Position.Bottom}
+            id="b"
+            isConnectable={isConnectable}
+          />
         </div>
-        <Handle
-          type="source"
-          position={Position.Bottom}
-          id="b"
-          isConnectable={isConnectable}
-        />
-      </div>
       </div>
     </>
   );
