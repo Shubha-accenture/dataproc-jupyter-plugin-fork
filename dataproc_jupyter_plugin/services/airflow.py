6bb86a0baafe7d04058fa1f78a69598c58f4722c
saranyaloganathan23 <saranya.lognathan@accenture.com>
2024-09-20 10:27:20 +0530

    harcoded payload for edit

--- dataproc_jupyter_plugin/services/airflow.py
+++ dataproc_jupyter_plugin/services/airflow.py
@@ -219,118 +219,284 @@ class Client:
             return {"error": str(e)}
 
     async def edit_jobs(self, dag_id, bucket_name):
-        try:
-            cluster_name = ""
-            serverless_name = ""
-            email_on_success = "False"
-            stop_cluster_check = "False"
-            mode_selected = "serverless"
-            time_zone = ""
-            pattern = r"parameters\s*=\s*'''(.*?)'''"
-            file_response = await self.get_dag_file(dag_id, bucket_name)
-            content_str = file_response.decode("utf-8")
-            file_content = re.sub(r"(?<!\\)\\(?!n)", "", content_str)
+        payload = {
+    "job_name": "PayloadModifiedtest",
+    "composer_environment_name": "multinode-scheduler",
+    "email_failure": False,
+    "email_delay": False,
+    "email_success": False,
+    "email_ids": [],
+    "nodes": [
+        {
+            "id": "1",
+            "type": "composerNode",
+            "position": {
+                "x": -60.99160589613848,
+                "y": -450.85951809498465
+            },
+            "data": {
+                "nodeType": "Trigger",
+                "retryCount": 0,
+                "retryDelay": 0,
+                "parameter": [],
+                "stopCluster": "",
+                "clusterName": "",
+                "serverless": "",
+                "scheduleValue": "",
+                "timeZone": "",
+                "datasetId": "",
+                "tableId": "",
+                "location": "",
+                "writeDisposition": "",
+                "serviceAccount": "",
+                "keyRings": "",
+                "kmsKey": "",
+                "isSaveQuery": False,
+                "isAutoRegion": True
+            },
+            "width": 232,
+            "height": 98
+        },
+        {
+            "id": "2",
+            "type": "composerNode",
+            "position": {
+                "x": -231.4928727243676,
+                "y": -251.4640478260099
+            },
+            "data": {
+                "nodeType": "Serverless",
+                "inputFile": "test1.ipynb",
+                "retryCount": 22,
+                "retryDelay": 22,
+                "parameter": [],
+                "serverless": {
+                    "name": "projects/dataproc-jupyter-extension-dev/locations/us-central1/sessionTemplates/runtime-00000323d751",
+                    "createTime": "2024-07-26T08:19:02.867092Z",
+                    "jupyterSession": {
+                        "kernel": "PYTHON",
+                        "displayName": "payload"
+                    },
+                    "creator": "aditeekatti@google.com",
+                    "labels": {
+                        "client": "dataproc-jupyter-plugin"
+                    },
+                    "runtimeConfig": {
+                        "version": "2.2",
+                        "properties": {
+                            "spark.driver.cores": "4",
+                            "spark.driver.memory": "12200m",
+                            "spark.driver.memoryOverhead": "1220m",
+                            "spark.dataproc.driver.disk.size": "400g",
+                            "spark.dataproc.driver.disk.tier": "standard",
+                            "spark.executor.cores": "12",
+                            "spark.executor.memory": "12200m",
+                            "spark.dataproc.executor.disk.tier": "premium",
+                            "spark.dataproc.executor.disk.size": "750g",
+                            "spark.executor.instances": "2",
+                            "spark.dynamicAllocation.enabled": "true",
+                            "spark.dynamicAllocation.initialExecutors": "2",
+                            "spark.dynamicAllocation.minExecutors": "2",
+                            "spark.dynamicAllocation.maxExecutors": "1000",
+                            "spark.dynamicAllocation.executorAllocationRatio": "0.3",
+                            "spark.reducer.fetchMigratedShuffle.enabled": "false",
+                            "spark.dataproc.driverEnv.LANG": "C.UTF-8",
+                            "spark.executorEnv.LANG": "C.UTF-8",
+                            "spark.dataproc.executor.compute.tier": "premium",
+                            "spark.dataproc.executor.resource.accelerator.type": "a100-40",
+                            "spark.plugins": "com.nvidia.spark.SQLPlugin",
+                            "spark.task.resource.gpu.amount": "0.25",
+                            "spark.shuffle.manager": "com.nvidia.spark.rapids.RapidsShuffleManager",
+                            "key": "value"
+                        }
+                    },
+                    "environmentConfig": {
+                        "executionConfig": {
+                            "subnetworkUri": "default"
+                        },
+                        "peripheralsConfig": {}
+                    },
+                    "description": "payload",
+                    "updateTime": "2024-07-26T08:19:02.867092Z",
+                    "uuid": "f1453a7d-5179-48ce-8156-c01220bf17ea"
+                },
+                "serviceAccount": "",
+                "stopCluster": "",
+                "clusterName": "",
+                "scheduleValue": "",
+                "timeZone": "",
+                "datasetId": "",
+                "tableId": "",
+                "location": "",
+                "writeDisposition": "",
+                "keyRings": "",
+                "kmsKey": "",
+                "isSaveQuery": False,
+                "isAutoRegion": True
+            },
+            "width": 232,
+            "height": 98,
+            "selected": False,
+            "dragging": False
+        },
+        {
+            "id": "3",
+            "type": "composerNode",
+            "position": {
+                "x": -230.29695888326685,
+                "y": -74.46879934309815
+            },
+            "data": {
+                "nodeType": "Cluster",
+                "inputFile": "test2.ipynb",
+                "retryCount": 44,
+                "retryDelay": 44,
+                "parameter": [],
+                "stopCluster": "",
+                "clusterName": "cluster-dpms-test",
+                "serverless": "",
+                "scheduleValue": "",
+                "timeZone": "",
+                "datasetId": "",
+                "tableId": "",
+                "location": "",
+                "writeDisposition": "",
+                "serviceAccount": "",
+                "keyRings": "",
+                "kmsKey": "",
+                "isSaveQuery": False,
+                "isAutoRegion": True
+            },
+            "width": 232,
+            "height": 98
+        }
+    ],
+    "edges": [
+        {
+            "id": "2",
+            "source": "1",
+            "target": "2"
+        },
+        {
+            "id": "3",
+            "source": "2",
+            "target": "3"
+        }
+    ]
+}
+        return payload
+        # try:
+        #     cluster_name = ""
+        #     serverless_name = ""
+        #     email_on_success = "False"
+        #     stop_cluster_check = "False"
+        #     mode_selected = "serverless"
+        #     time_zone = ""
+        #     pattern = r"parameters\s*=\s*'''(.*?)'''"
+        #     file_response = await self.get_dag_file(dag_id, bucket_name)
+        #     content_str = file_response.decode("utf-8")
+        #     file_content = re.sub(r"(?<!\\)\\(?!n)", "", content_str)
 
-            if file_content:
-                for line in file_content.split("\n"):
-                    if "input_notebook" in line:
-                        input_notebook = line.split("=")[-1].strip().strip("'\"")
-                        break
+        #     if file_content:
+        #         for line in file_content.split("\n"):
+        #             if "input_notebook" in line:
+        #                 input_notebook = line.split("=")[-1].strip().strip("'\"")
+        #                 break
 
-                for line in file_content.split("\n"):
-                    match = re.search(pattern, file_content, re.DOTALL)
-                    if match:
-                        parameters_yaml = match.group(1)
-                        parameters_list = [
-                            line.strip()
-                            for line in parameters_yaml.split("\n")
-                            if line.strip()
-                        ]
-                    else:
-                        parameters_list = []
+        #         for line in file_content.split("\n"):
+        #             match = re.search(pattern, file_content, re.DOTALL)
+        #             if match:
+        #                 parameters_yaml = match.group(1)
+        #                 parameters_list = [
+        #                     line.strip()
+        #                     for line in parameters_yaml.split("\n")
+        #                     if line.strip()
+        #                 ]
+        #             else:
+        #                 parameters_list = []
 
-                for line in file_content.split("\n"):
-                    if "email" in line:
-                        # Extract the email string from the line
-                        email_str = (
-                            line.split(":")[-1].strip().strip("'\"").replace(",", "")
-                        )
-                        # Use regular expression to extract email addresses
-                        email_list = re.findall(
-                            r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
-                            email_str,
-                        )
-                        # Remove quotes from the email addresses
-                        email_list = [email.strip("'\"") for email in email_list]
-                        break
-                for line in file_content.split("\n"):
-                    if "cluster_name" in line:
-                        cluster_name = (
-                            line.split(":")[-1]
-                            .strip()
-                            .strip("'\"}")
-                            .split("'")[0]
-                            .strip()
-                        )  # Extract project_id from the line
-                    elif "submit_pyspark_job" in line:
-                        mode_selected = "cluster"
-                    elif "'retries'" in line:
-                        retries = line.split(":")[-1].strip().strip("'\"},")
-                        retry_count = int(
-                            retries.strip("'\"")
-                        )  # Extract retry_count from the line
-                    elif "retry_delay" in line:
-                        retry_delay = int(
-                            line.split("int('")[1].split("')")[0]
-                        )  # Extract retry_delay from the line
-                    elif "email_on_failure" in line:
-                        second_part = line.split(":")[1].strip()
-                        email_on_failure = second_part.split("'")[
-                            1
-                        ]  # Extract email_failure from the line
-                    elif "email_on_retry" in line:
-                        second_part = line.split(":")[1].strip()
-                        email_on_retry = second_part.split("'")[1]
-                    elif "email_on_success" in line:
-                        second_part = line.split(":")[1].strip()
-                        email_on_success = second_part.split("'")[1]
-                    elif "schedule_interval" in line:
-                        schedule_interval = (
-                            line.split("=")[-1]
-                            .strip()
-                            .strip("'\"")
-                            .split(",")[0]
-                            .rstrip("'\"")
-                        )  # Extract schedule_interval from the line
-                    elif "stop_cluster_check" in line:
-                        stop_cluster_check = line.split("=")[-1].strip().strip("'\"")
-                    elif "serverless_name" in line:
-                        serverless_name = line.split("=")[-1].strip().strip("'\"")
-                    elif "time_zone" in line:
-                        time_zone = line.split("=")[-1].strip().strip("'\"")
+        #         for line in file_content.split("\n"):
+        #             if "email" in line:
+        #                 # Extract the email string from the line
+        #                 email_str = (
+        #                     line.split(":")[-1].strip().strip("'\"").replace(",", "")
+        #                 )
+        #                 # Use regular expression to extract email addresses
+        #                 email_list = re.findall(
+        #                     r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
+        #                     email_str,
+        #                 )
+        #                 # Remove quotes from the email addresses
+        #                 email_list = [email.strip("'\"") for email in email_list]
+        #                 break
+        #         for line in file_content.split("\n"):
+        #             if "cluster_name" in line:
+        #                 cluster_name = (
+        #                     line.split(":")[-1]
+        #                     .strip()
+        #                     .strip("'\"}")
+        #                     .split("'")[0]
+        #                     .strip()
+        #                 )  # Extract project_id from the line
+        #             elif "submit_pyspark_job" in line:
+        #                 mode_selected = "cluster"
+        #             elif "'retries'" in line:
+        #                 retries = line.split(":")[-1].strip().strip("'\"},")
+        #                 retry_count = int(
+        #                     retries.strip("'\"")
+        #                 )  # Extract retry_count from the line
+        #             elif "retry_delay" in line:
+        #                 retry_delay = int(
+        #                     line.split("int('")[1].split("')")[0]
+        #                 )  # Extract retry_delay from the line
+        #             elif "email_on_failure" in line:
+        #                 second_part = line.split(":")[1].strip()
+        #                 email_on_failure = second_part.split("'")[
+        #                     1
+        #                 ]  # Extract email_failure from the line
+        #             elif "email_on_retry" in line:
+        #                 second_part = line.split(":")[1].strip()
+        #                 email_on_retry = second_part.split("'")[1]
+        #             elif "email_on_success" in line:
+        #                 second_part = line.split(":")[1].strip()
+        #                 email_on_success = second_part.split("'")[1]
+        #             elif "schedule_interval" in line:
+        #                 schedule_interval = (
+        #                     line.split("=")[-1]
+        #                     .strip()
+        #                     .strip("'\"")
+        #                     .split(",")[0]
+        #                     .rstrip("'\"")
+        #                 )  # Extract schedule_interval from the line
+        #             elif "stop_cluster_check" in line:
+        #                 stop_cluster_check = line.split("=")[-1].strip().strip("'\"")
+        #             elif "serverless_name" in line:
+        #                 serverless_name = line.split("=")[-1].strip().strip("'\"")
+        #             elif "time_zone" in line:
+        #                 time_zone = line.split("=")[-1].strip().strip("'\"")
 
-                payload = {
-                    "input_filename": input_notebook,
-                    "parameters": parameters_list,
-                    "mode_selected": mode_selected,
-                    "cluster_name": cluster_name,
-                    "serverless_name": serverless_name,
-                    "retry_count": retry_count,
-                    "retry_delay": retry_delay,
-                    "email_failure": email_on_failure,
-                    "email_delay": email_on_retry,
-                    "email_success": email_on_success,
-                    "email": email_list,
-                    "schedule_value": schedule_interval,
-                    "stop_cluster": stop_cluster_check,
-                    "time_zone": time_zone,
-                }
-                return payload
+        #         payload = {
+        #             "input_filename": input_notebook,
+        #             "parameters": parameters_list,
+        #             "mode_selected": mode_selected,
+        #             "cluster_name": cluster_name,
+        #             "serverless_name": serverless_name,
+        #             "retry_count": retry_count,
+        #             "retry_delay": retry_delay,
+        #             "email_failure": email_on_failure,
+        #             "email_delay": email_on_retry,
+        #             "email_success": email_on_success,
+        #             "email": email_list,
+        #             "schedule_value": schedule_interval,
+        #             "stop_cluster": stop_cluster_check,
+        #             "time_zone": time_zone,
+        #         }
+        #         return payload
 
-            else:
-                self.log.exception("No Dag file found")
-        except Exception as e:
-            self.log.exception(f"Error downloading dag file: {str(e)}")
+        #     else:
+        #         self.log.exception("No Dag file found")
+        # except Exception as e:
+        #     self.log.exception(f"Error downloading dag file: {str(e)}")
 
     async def list_import_errors(self, composer):
         airflow_uri, bucket = await self.get_airflow_uri(composer)
